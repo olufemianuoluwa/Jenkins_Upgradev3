4610d2a4e6c276c4271775e430abfdda0e9b2c74
olufemianuoluwa <olufemi.ogunsola@outlook.com>
2022-10-08 15:25:54 -0400

    fhoeubfe

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -40,4 +40,10 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+   @Test
+  public void greetShouldIncludeGreetingMessage() {
+    String someone = "JENKINS LEARNING";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
 }
